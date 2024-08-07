40bf342f5ed0ce36eff9fd947c7095dbe37d1205
mujibbur <mujibbur.rohman@gmail.com>
2024-08-07 14:35:54 +0700

    Tugas pemrograman visual pert.1 sd prtm 4

---
+++ Pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author SERVER LAB1- RPL K4
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Percobaan modul=new Percobaan();
+//        modul.setVisible(true);
+//        Pertemuan2 modul=new Pertemuan2();
+//        modul.setVisible(true);
+//        Pertemuan3 modul = new Pertemuan3();
+//        modul.setVisible(true);
+        Pertemuan4 modul = new Pertemuan4();
+        modul.setVisible(true);
+    }
+
+}
